a9d2250cc560765576e1396b0facd24b100ac35c
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-01-19 15:25:50 +0100

    [DS-3782] Upgrade Commons Collections to version 4

--- dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/ItemServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.dspace.app.util.AuthorizeUtil;
