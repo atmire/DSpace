1829b5c0494968088a434f65af587dcf40b5d7d0
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-05-15 08:44:17 +0200

    Made the requestUrl part configurable

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/RootRestResourceController.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/RootRestResourceController.java
@@ -45,21 +45,10 @@ public class RootRestResourceController {
     @RequestMapping(method = RequestMethod.GET)
     public RootResource listDefinedEndpoint(HttpServletRequest request) {
 
-
-        String restUrl = getRestURL(request);
-
-        log.info("The editted URL from the getRestUrl method in RootRestResourceController is: " + restUrl);
-
-        RootRest rootRest = rootRestRepository.getRoot(restUrl);
+        RootRest rootRest = rootRestRepository.getRoot();
         RootResource rootResource = new RootResource(rootRest);
         halLinkService.addLinks(rootResource);
 
         return rootResource;
-    }
-
-    private String getRestURL(HttpServletRequest request) {
-        String url = request.getRequestURL().toString();
-        log.info("The URL retrieved from the request.getRequestUrl is: " + url);
-        return url.substring(0, url.length() - request.getRequestURI().length()) + request.getContextPath();
     }
 }
