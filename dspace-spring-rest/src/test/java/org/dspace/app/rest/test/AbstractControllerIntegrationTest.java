6006d7f38dd0e8363f6a2536b56b3b5983cb2325
Tom Desair <tom.desair@atmire.com>
2018-08-22 14:34:18 +0200

    DS-3542: Spring Authorization - @Preauthorization

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/test/AbstractControllerIntegrationTest.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/test/AbstractControllerIntegrationTest.java
@@ -64,7 +64,7 @@ import org.springframework.web.context.WebApplicationContext;
 public class AbstractControllerIntegrationTest extends AbstractIntegrationTestWithDatabase {
 
     protected static final String AUTHORIZATION_HEADER = "Authorization";
-    protected static final String AUTHORIZATION_TYPE = "Bearer";
+    protected static final String AUTHORIZATION_TYPE = "Bearer ";
 
     protected static final String REQUEST_SCHEME = "http";
     protected static final String REQUEST_NAME = "localhost";
@@ -119,7 +119,8 @@ public class AbstractControllerIntegrationTest extends AbstractIntegrationTestWi
                 return request;
             });
         if (StringUtils.isNotBlank(authToken)) {
-            defaultRequestParameters.header(AUTHORIZATION_HEADER, AUTHORIZATION_TYPE + " " + authToken);
+            mockMvcBuilder.defaultRequest(
+                    get("").header(AUTHORIZATION_HEADER, AUTHORIZATION_TYPE + authToken));
         }
         mockMvcBuilder.defaultRequest(defaultRequestParameters);
 
@@ -135,7 +136,9 @@ public class AbstractControllerIntegrationTest extends AbstractIntegrationTestWi
     }
 
     public String getAuthToken(String user, String password) throws Exception {
-        return getAuthResponse(user, password).getHeader(AUTHORIZATION_HEADER);
+        return StringUtils.substringAfter(
+                getAuthResponse(user, password).getHeader(AUTHORIZATION_HEADER),
+                AUTHORIZATION_TYPE);
     }
 
     public String getPatchContent(List<Operation> ops) {
