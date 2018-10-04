e4c576f4353602af545e44678bd612315ad0a051
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-10-04 15:16:22 +0200

    constructed the endpoint to retrieve a list of mapped items for a collection

---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/MappingItemRestWrapper.java
@@ -0,0 +1,43 @@
+package org.dspace.app.rest.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.dspace.app.rest.MappingItemRestController;
+
+public class MappingItemRestWrapper implements RestAddressableModel {
+
+    @JsonIgnore
+    private List<ItemRest> mappingItemRestList;
+
+    private UUID collectionUuid;
+
+    public UUID getCollectionUuid() {
+        return collectionUuid;
+    }
+
+    public void setCollectionUuid(UUID collectionUuid) {
+        this.collectionUuid = collectionUuid;
+    }
+
+    public List<ItemRest> getMappingItemRestList() {
+        return mappingItemRestList;
+    }
+
+    public void setMappingItemRestList(List<ItemRest> mappingItemRestList) {
+        this.mappingItemRestList = mappingItemRestList;
+    }
+
+    public String getCategory() {
+        return "core";
+    }
+
+    public Class getController() {
+        return MappingItemRestController.class;
+    }
+
+    public String getType() {
+        return "collection";
+    }
+}
---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/MappingItemResourceWrapper.java
@@ -0,0 +1,44 @@
+package org.dspace.app.rest.model.hateoas;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.dspace.app.rest.model.ItemRest;
+import org.dspace.app.rest.model.MappingItemRestWrapper;
+import org.dspace.app.rest.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MappingItemResourceWrapper extends HALResource<MappingItemRestWrapper> {
+    private static final Logger log = LoggerFactory.getLogger(MappingItemResourceWrapper.class);
+
+    @JsonIgnore
+    private List<ItemResource> itemResources;
+    @JsonIgnore
+    private Integer totalElements;
+
+    public MappingItemResourceWrapper(MappingItemRestWrapper content, Utils utils,
+                                      Integer totalElements, String... rels) {
+        super(content);
+        embed(utils);
+        this.totalElements = totalElements;
+    }
+
+    private void embed(Utils utils) {
+        List<ItemResource> itemResources = new LinkedList<>();
+        for (ItemRest itemRest : getContent().getMappingItemRestList()) {
+            itemResources.add(new ItemResource(itemRest, utils));
+        }
+        this.itemResources = itemResources;
+        embedResource("mappingItems", itemResources);
+    }
+
+    public List<ItemResource> getItemResources() {
+        return itemResources;
+    }
+
+    public Integer getTotalElements() {
+        return totalElements;
+    }
+}
