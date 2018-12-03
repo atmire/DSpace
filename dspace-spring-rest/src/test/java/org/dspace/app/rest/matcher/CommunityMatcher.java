1f31259afcce30e5e286c6f30e18d8c5b603cbc9
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2018-12-03 10:37:46 +0100

    [Task 57834] implemented the CRUD for the collections in CollectionRestRepository

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/CommunityMatcher.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/matcher/CommunityMatcher.java
@@ -11,6 +11,7 @@ import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 import org.dspace.content.Collection;
@@ -53,12 +54,13 @@ public class CommunityMatcher {
     }
 
     public static Matcher<? super Object> matchCommunityWithCollectionEntry(String name, UUID uuid, String handle,
-                                                                            Collection col) {
+                                                                            Collection col) throws SQLException {
         return allOf(
             matchProperties(name, uuid, handle),
             hasJsonPath("$._embedded.collections._embedded.collections[0]",
                         CollectionMatcher
-                            .matchCollectionEntry(col.getName(), col.getID(), col.getHandle(), col.getLogo())),
+                            .matchCollectionEntry(col.getName(), col.getID(), col.getHandle(),
+                                                  col.getLogo(), col.getCommunities().get(0).getID())),
             hasJsonPath("$._embedded.logo", Matchers.not(Matchers.empty())),
             matchLinks(uuid)
         );
