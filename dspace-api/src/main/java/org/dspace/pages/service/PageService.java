c9009f997bd2d67b45f7daab5eb2320c7d592d6f
Raf Ponsaerts <raf.ponsaerts@atmire.com>
2019-02-19 14:03:04 +0100

    Applied feedback

--- dspace-api/src/main/java/org/dspace/pages/service/PageService.java
+++ dspace-api/src/main/java/org/dspace/pages/service/PageService.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.content.Bitstream;
 import org.dspace.core.Context;
 import org.dspace.pages.Page;
 import org.dspace.service.DSpaceCRUDService;
@@ -29,11 +28,12 @@ public interface PageService extends DSpaceCRUDService<Page> {
      * @param context       The relevant DSpace context
      * @param name          The name that the newly created page object will have in the database
      * @param language      The language that the newly created page object will have in the database
-     * @param bitstream     The bitstream that the newly created page object will have in the database
-     * @return          The created Page object
+     * @param inputStream   The inputStream that will be used to create the bitstream that the newly created page
+     *                      object will have in the database
+     * @return              The created Page object
      * @throws SQLException If something goes wrong
      */
-    public Page create(Context context, String name, String language, Bitstream bitstream) throws SQLException;
+    public Page create(Context context, String name, String language, InputStream inputStream) throws SQLException;
 
     /**
      * This method will find a Page object by UUID and return it
