58358352be40db58b52cb9f0da63ae0f1cdb8b6f
Ben Bosman <ben@atmire.com>
2019-03-14 12:52:18 +0100

    tests with context

--- dspace-spring-rest/src/test/java/org/dspace/app/rest/test/AbstractEntityIntegrationTest.java
+++ dspace-spring-rest/src/test/java/org/dspace/app/rest/test/AbstractEntityIntegrationTest.java
@@ -10,9 +10,14 @@ package org.dspace.app.rest.test;
 import org.dspace.app.rest.builder.EntityTypeBuilder;
 import org.dspace.app.rest.builder.RelationshipTypeBuilder;
 import org.dspace.content.EntityType;
+import org.dspace.content.service.EntityTypeService;
 import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AbstractEntityIntegrationTest extends AbstractControllerIntegrationTest {
+
+    @Autowired
+    private EntityTypeService entityTypeService;
 
     /**
      * This method will call the setUp method from AbstractControllerIntegrationTest.
@@ -35,58 +40,63 @@ public class AbstractEntityIntegrationTest extends AbstractControllerIntegration
     public void setUp() throws Exception {
         super.setUp();
 
-        context.turnOffAuthorisationSystem();
+        if (entityTypeService.findAll(context).size() == 0) {
 
-        EntityType publication = EntityTypeBuilder.createEntityTypeBuilder(context, "Publication").build();
-        EntityType person = EntityTypeBuilder.createEntityTypeBuilder(context, "Person").build();
-        EntityType project = EntityTypeBuilder.createEntityTypeBuilder(context, "Project").build();
-        EntityType orgUnit = EntityTypeBuilder.createEntityTypeBuilder(context, "OrgUnit").build();
-        EntityType journal = EntityTypeBuilder.createEntityTypeBuilder(context, "Journal").build();
-        EntityType journalVolume = EntityTypeBuilder.createEntityTypeBuilder(context, "JournalVolume").build();
-        EntityType journalIssue = EntityTypeBuilder.createEntityTypeBuilder(context, "JournalIssue").build();
+            context.turnOffAuthorisationSystem();
+
+            EntityType publication = EntityTypeBuilder.createEntityTypeBuilder(context, "Publication").build();
+            EntityType person = EntityTypeBuilder.createEntityTypeBuilder(context, "Person").build();
+            EntityType project = EntityTypeBuilder.createEntityTypeBuilder(context, "Project").build();
+            EntityType orgUnit = EntityTypeBuilder.createEntityTypeBuilder(context, "OrgUnit").build();
+            EntityType journal = EntityTypeBuilder.createEntityTypeBuilder(context, "Journal").build();
+            EntityType journalVolume = EntityTypeBuilder.createEntityTypeBuilder(context, "JournalVolume").build();
+            EntityType journalIssue = EntityTypeBuilder.createEntityTypeBuilder(context, "JournalIssue").build();
 
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, person, "isAuthorOfPublication",
-                                                              "isPublicationOfAuthor", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, person, "isAuthorOfPublication",
+                    "isPublicationOfAuthor", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, project, "isProjectOfPublication",
-                                                              "isPublicationOfProject", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, project, "isProjectOfPublication",
+                    "isPublicationOfProject", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, orgUnit, "isOrgUnitOfPublication",
-                                                              "isPublicationOfOrgUnit", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, orgUnit, "isOrgUnitOfPublication",
+                    "isPublicationOfOrgUnit", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, person, project, "isProjectOfPerson",
-                                                              "isPersonOfProject", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, person, project, "isProjectOfPerson",
+                    "isPersonOfProject", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, person, orgUnit, "isOrgUnitOfPerson",
-                                                              "isPersonOfOrgUnit", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, person, orgUnit, "isOrgUnitOfPerson",
+                    "isPersonOfOrgUnit", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, project, orgUnit, "isOrgUnitOfProject",
-                                                              "isProjectOfOrgUnit", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, project, orgUnit, "isOrgUnitOfProject",
+                    "isProjectOfOrgUnit", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journal, journalVolume, "isVolumeOfJournal",
-                                                              "isJournalOfVolume", 0, Integer.MAX_VALUE, 1,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journal, journalVolume, "isVolumeOfJournal",
+                    "isJournalOfVolume", 0, Integer.MAX_VALUE, 1,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journalVolume, journalIssue,
-                                                              "isIssueOfJournalVolume", "isJournalVolumeOfIssue", 0,
-                                                              Integer.MAX_VALUE, 1,
-                                                              1).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journalVolume, journalIssue,
+                    "isIssueOfJournalVolume", "isJournalVolumeOfIssue", 0,
+                    Integer.MAX_VALUE, 1,
+                    1).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, orgUnit, "isAuthorOfPublication",
-                                                              "isPublicationOfAuthor", 0, Integer.MAX_VALUE, 0,
-                                                              Integer.MAX_VALUE).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, publication, orgUnit, "isAuthorOfPublication",
+                    "isPublicationOfAuthor", 0, Integer.MAX_VALUE, 0,
+                    Integer.MAX_VALUE).build();
 
-        RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journalIssue, publication,
-                                                              "isPublicationOfJournalIssue",
-                                                              "isJournalIssueOfPublication", 0, Integer.MAX_VALUE, 0,
-                                                              1).build();
+            RelationshipTypeBuilder.createRelationshipTypeBuilder(context, journalIssue, publication,
+                    "isPublicationOfJournalIssue",
+                    "isJournalIssueOfPublication", 0, Integer.MAX_VALUE, 0,
+                    1).build();
+
+            context.restoreAuthSystemState();
+        }
     }
 
 
