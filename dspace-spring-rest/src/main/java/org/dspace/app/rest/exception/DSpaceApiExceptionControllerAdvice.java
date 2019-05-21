a8031df49f8ce53c526552b83763633adab8e6e2
Marie Verdonck <maria.verdonck@atmire.com>
2019-05-21 16:42:21 +0200

    Error & Status for non-successful POST/DELETE mappedCollections requests - POST /api/core/items/<:uuid>/mappedCollections  > 405: item is a template item  > 422: specified collection not found or is owningCollection of item - DELETE /api/core/items/<:uuid>/mappedCollections/<:collection_uuid>  > 405: item is a template item  > 422: specified collection not found or is owningCollection of item

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/exception/DSpaceApiExceptionControllerAdvice.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/exception/DSpaceApiExceptionControllerAdvice.java
@@ -75,6 +75,23 @@ public class DSpaceApiExceptionControllerAdvice extends ResponseEntityExceptionH
                           HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(MethodNotAllowedException.class)
+    protected void methodNotAllowedException(HttpServletRequest request, HttpServletResponse response,
+                                                  Exception ex) throws IOException {
+        sendErrorResponse(request, response, ex, ex.getMessage(), HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler( {UnprocessableEntityException.class})
+    protected void handleUnprocessableEntityException(HttpServletRequest request, HttpServletResponse response,
+                                                      Exception ex) throws IOException {
+
+        //422 is not defined in HttpServletResponse.  Its meaning is "Unprocessable Entity".
+        //Using the value from HttpStatus.
+        sendErrorResponse(request, response, null,
+                ex.getMessage(),
+                HttpStatus.UNPROCESSABLE_ENTITY.value());
+    }
+
     @ExceptionHandler( {MissingParameterException.class, QueryMethodParameterConversionException.class})
     protected void ParameterConversionException(HttpServletRequest request, HttpServletResponse response, Exception ex)
         throws IOException {
