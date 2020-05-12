73466b21b3909bf1268a49aa0044f397f3f04ca9
Marie Verdonck <maria.verdonck@atmire.com>
2020-05-12 17:25:23 +0200

    70852: live import plugin for BTE: basic metadata support

--- dspace-api/src/main/java/org/dspace/external/provider/metadata/impl/BTEMetadataSuggestionProvider.java
+++ dspace-api/src/main/java/org/dspace/external/provider/metadata/impl/BTEMetadataSuggestionProvider.java
@@ -12,19 +12,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import gr.ekt.bte.core.DataLoader;
 import gr.ekt.bte.core.Record;
 import gr.ekt.bte.core.RecordSet;
 import gr.ekt.bte.core.Value;
 import gr.ekt.bte.dataloader.FileDataLoader;
 import gr.ekt.bte.exceptions.MalformedSourceException;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpException;
 import org.apache.logging.log4j.Logger;
 import org.dspace.app.itemimport.BTEBatchImportService;
 import org.dspace.content.Bitstream;
+import org.dspace.content.Item;
+import org.dspace.content.MetadataValue;
 import org.dspace.content.dto.MetadataValueDTO;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.external.model.ExternalDataObject;
 import org.dspace.external.provider.impl.BTEDataProvider;
@@ -32,6 +41,8 @@ import org.dspace.external.provider.metadata.MetadataSuggestionProvider;
 import org.dspace.services.factory.DSpaceServicesFactory;
 import org.dspace.storage.bitstore.factory.StorageServiceFactory;
 import org.dspace.storage.bitstore.service.BitstreamStorageService;
+import org.dspace.submit.listener.MetadataListener;
+import org.dspace.submit.lookup.SubmissionLookupDataLoader;
 import org.dspace.utils.StreamUtils;
 
 /**
@@ -47,14 +58,24 @@ public class BTEMetadataSuggestionProvider extends MetadataSuggestionProvider<BT
      */
     private static Logger log = org.apache.logging.log4j.LogManager.getLogger(BTEMetadataSuggestionProvider.class);
 
+    /**
+     * Field name in a {@link Record} from a {@link DataLoader} where the title is in
+     */
     private String fieldForTitle;
-    protected BitstreamStorageService bitstreamStorageService = StorageServiceFactory.getInstance()
-                                                                                     .getBitstreamStorageService();
-    protected BTEBatchImportService bteBatchImportService = DSpaceServicesFactory.getInstance().getServiceManager()
-                                                                                 .getServiceByName(
-                                                                                     "org.dspace.app.itemimport" +
-                                                                                     ".BTEBatchImportService",
-                                                                                     BTEBatchImportService.class);
+
+    // Services
+    protected BitstreamStorageService bitstreamStorageService
+        = StorageServiceFactory.getInstance().getBitstreamStorageService();
+
+    protected BTEBatchImportService bteBatchImportService
+        = DSpaceServicesFactory.getInstance().getServiceManager()
+                               .getServiceByName("org.dspace.app.itemimport.BTEBatchImportService",
+                                                 BTEBatchImportService.class);
+
+    protected List<MetadataListener> listeners = DSpaceServicesFactory.getInstance().getServiceManager()
+                                                                      .getServicesByType(MetadataListener.class);
+
+    protected final ItemService itemService = ContentServiceFactory.getInstance().getItemService();
 
     @Override
     public List<ExternalDataObject> bitstreamQuery(Bitstream bitstream) {
@@ -82,6 +103,68 @@ public class BTEMetadataSuggestionProvider extends MetadataSuggestionProvider<BT
         return new ArrayList<>();
     }
 
+    @Override
+    public List<ExternalDataObject> metadataQuery(Item item, int start, int limit) {
+        List<ExternalDataObject> result = new ArrayList<>();
+        for (MetadataListener listener : listeners) {
+            for (DataLoader dataLoader : listener.getDataloadersMap().values()) {
+                SubmissionLookupDataLoader submissionLookupDataLoader = (SubmissionLookupDataLoader) dataLoader;
+                Map<String, Set<String>> identifierKeys = this.getIdentifierKeysMap(item, listener);
+                try (Context context = new Context()) {
+                    context.turnOffAuthorisationSystem();
+                    List<Record> records = submissionLookupDataLoader.getByIdentifier(context, identifierKeys);
+                    result.addAll(this.constructExternalDataObjectListFromRecordList(records));
+                } catch (HttpException | IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Creates the mapping between a key name (ex. doi) and a set of values corresponding with that key from the
+     * item's metadata value (all it's values in dc.identifier.doi)
+     *
+     * @param item     Item with metadata containing identifiers
+     * @param listener Config bean with the mapping between key name and the corresponding mdField string
+     * @return Mapping between a key name and the corresponding set of values of dat key in the given item's metadata
+     */
+    private Map<String, Set<String>> getIdentifierKeysMap(Item item, MetadataListener listener) {
+        Map<String, Set<String>> identifierKeys = new HashMap<>();
+        for (Map.Entry<String, String> mdFieldToIdentifierFieldName : listener.getMetadata().entrySet()) {
+            List<MetadataValue> metadataValues =
+                itemService.getMetadataByMetadataString(item, mdFieldToIdentifierFieldName.getKey());
+
+            if (!metadataValues.isEmpty()) {
+                Set<String> keySet = new HashSet<>();
+                for (MetadataValue metadataValue : metadataValues) {
+                    keySet.add(metadataValue.getValue());
+                }
+                identifierKeys.put(mdFieldToIdentifierFieldName.getValue(), keySet);
+            }
+        }
+        return identifierKeys;
+    }
+
+    /**
+     * Constructs the external data object list based on a record list.
+     * Each external data object's metadata gets populated from a record in the list (see @link this
+     * .fillInExternalDataObjectMdWithRecord()} and the display value is extracted from the record with
+     * {@link this.fieldForTile} as fieldName in the record.
+     *
+     * @param records List of records to be converted to list of external data objects
+     * @return List of external data objects, populate with data from the given list of records
+     */
+    private List<ExternalDataObject> constructExternalDataObjectListFromRecordList(List<Record> records) {
+        List<ExternalDataObject> externalDataObjects = new ArrayList<>();
+        for (Record rec : records) {
+            externalDataObjects.add(constructExternalDataObjectFromRecord(rec));
+        }
+        return externalDataObjects;
+    }
+
+
     /**
      * Constructs the external data object list based on a record set.
      * Each external data object's metadata gets populated from a record in the set (see @link this
@@ -94,19 +177,32 @@ public class BTEMetadataSuggestionProvider extends MetadataSuggestionProvider<BT
     private List<ExternalDataObject> constructExternalDataObjectListFromRecordSet(RecordSet records) {
         List<ExternalDataObject> externalDataObjects = new ArrayList<>();
         for (Record rec : records) {
-            ExternalDataObject externalDataObject = new ExternalDataObject();
-            if (super.getExternalDataProvider() != null &&
-                StringUtils.isNotBlank(super.getExternalDataProvider().getSourceIdentifier())) {
-                externalDataObject.setSource(super.getExternalDataProvider().getSourceIdentifier());
-            }
-            List<Value> titles = this.findTitlesInRecord(rec);
-            if (titles != null && !titles.isEmpty()) {
-                externalDataObject.setDisplayValue(titles.get(0).getAsString());
-                externalDataObject.setValue(titles.get(0).getAsString());
-            }
-            externalDataObjects.add(this.fillInExternalDataObjectMdWithRecord(rec, externalDataObject));
+            externalDataObjects.add(constructExternalDataObjectFromRecord(rec));
         }
         return externalDataObjects;
+    }
+
+    /**
+     * Constructs a single external data object from a single record.
+     * The external data object's metadata gets populated from a record (see @link this
+     * .fillInExternalDataObjectMdWithRecord()} and the display value is extracted from the record with
+     * {@link this.fieldForTile} as fieldName in the record.
+     *
+     * @param record Record to be converted in an external data object
+     * @return External data object corresponding to the given record
+     */
+    private ExternalDataObject constructExternalDataObjectFromRecord(Record record) {
+        ExternalDataObject externalDataObject = new ExternalDataObject();
+        if (super.getExternalDataProvider() != null &&
+            StringUtils.isNotBlank(super.getExternalDataProvider().getSourceIdentifier())) {
+            externalDataObject.setSource(super.getExternalDataProvider().getSourceIdentifier());
+        }
+        List<Value> titles = this.findTitlesInRecord(record);
+        if (titles != null && !titles.isEmpty()) {
+            externalDataObject.setDisplayValue(titles.get(0).getAsString());
+            externalDataObject.setValue(titles.get(0).getAsString());
+        }
+        return this.fillInExternalDataObjectMdWithRecord(record, externalDataObject);
     }
 
     /**
--- dspace-api/src/test/java/org/dspace/external/provider/metadata/impl/BTEMetadataSuggestionProviderTest.java
+++ dspace-api/src/test/java/org/dspace/external/provider/metadata/impl/BTEMetadataSuggestionProviderTest.java
@@ -18,10 +18,25 @@ import java.util.List;
 
 import org.apache.logging.log4j.Logger;
 import org.dspace.AbstractUnitTest;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Bitstream;
+import org.dspace.content.Collection;
+import org.dspace.content.Community;
+import org.dspace.content.Item;
+import org.dspace.content.MetadataField;
+import org.dspace.content.MetadataSchemaEnum;
+import org.dspace.content.NonUniqueMetadataException;
+import org.dspace.content.WorkspaceItem;
 import org.dspace.content.dto.MetadataValueDTO;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.service.BitstreamService;
+import org.dspace.content.service.CollectionService;
+import org.dspace.content.service.CommunityService;
+import org.dspace.content.service.InstallItemService;
+import org.dspace.content.service.ItemService;
+import org.dspace.content.service.MetadataFieldService;
+import org.dspace.content.service.MetadataSchemaService;
+import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.external.model.ExternalDataObject;
 import org.dspace.services.factory.DSpaceServicesFactory;
 import org.junit.After;
@@ -43,7 +58,20 @@ public class BTEMetadataSuggestionProviderTest extends AbstractUnitTest {
     private static Bitstream bitstream;
     private static File file;
 
+    private static Collection collection;
+    private static Community owningCommunity;
+    private static Item doiItem;
+
+    private static MetadataField doiMdField;
+
     protected BitstreamService bitstreamService;
+    protected ItemService itemService;
+    protected CommunityService communityService;
+    protected CollectionService collectionService;
+    protected WorkspaceItemService workspaceItemService;
+    protected InstallItemService installItemService;
+    protected MetadataFieldService metadataFieldService;
+    protected MetadataSchemaService metadataSchemaService;
 
     private BTEMetadataSuggestionProvider bteMetadataSuggestionProvider;
 
@@ -61,23 +89,53 @@ public class BTEMetadataSuggestionProviderTest extends AbstractUnitTest {
     @Override
     public void init() {
         super.init();
+        // Initialize services
         bitstreamService = ContentServiceFactory.getInstance().getBitstreamService();
+        itemService = ContentServiceFactory.getInstance().getItemService();
+        communityService = ContentServiceFactory.getInstance().getCommunityService();
+        collectionService = ContentServiceFactory.getInstance().getCollectionService();
+        workspaceItemService = ContentServiceFactory.getInstance().getWorkspaceItemService();
+        installItemService = ContentServiceFactory.getInstance().getInstallItemService();
+        metadataFieldService = ContentServiceFactory.getInstance().getMetadataFieldService();
+        metadataSchemaService = ContentServiceFactory.getInstance().getMetadataSchemaService();
         bteMetadataSuggestionProvider =
             DSpaceServicesFactory.getInstance().getServiceManager()
                                  .getServicesByType(BTEMetadataSuggestionProvider.class).get(0);
+
         context.turnOffAuthorisationSystem();
 
         file = new File(testProps.get("test.bitstream.ris").toString());
         try {
+            this.doiMdField = metadataFieldService.findByElement(context,
+                                                                 MetadataSchemaEnum.DC.getName(), "identifier", "doi");
+            if (this.doiMdField == null) {
+                context.turnOffAuthorisationSystem();
+                this.doiMdField = metadataFieldService
+                    .create(context, metadataSchemaService.find(context, MetadataSchemaEnum.DC.getName()),
+                            "identifier", "doi", "DOI");
+                context.restoreAuthSystemState();
+            }
             bitstream = bitstreamService.create(context, new FileInputStream(file));
+            this.owningCommunity = communityService.create(null, context);
+            this.collection = collectionService.create(context, owningCommunity);
+            WorkspaceItem workspaceItem = workspaceItemService.create(context, collection, true);
+            this.doiItem = installItemService.installItem(context, workspaceItem);
+            this.itemService.addMetadata(context, this.doiItem, "dc", "identifier", "doi", Item.ANY,
+                                         "10.1016/j.ctrv.2019.01.001");
         } catch (IOException e) {
             log.error("IO Error in init", e);
             fail("IO Error in init: " + e.getMessage());
         } catch (SQLException e) {
             log.error("SQL Error in init", e);
             fail("SQL Error in init: " + e.getMessage());
+        } catch (AuthorizeException e) {
+            log.error("Authorize Error in init", e);
+            fail("Authorize Error in init: " + e.getMessage());
+        } catch (NonUniqueMetadataException e) {
+            log.error("NonUniqueMetadata Error in init", e);
+            fail("NonUniqueMetadata Error in init: " + e.getMessage());
         }
-
+        context.restoreAuthSystemState();
     }
 
     /**
@@ -116,6 +174,30 @@ public class BTEMetadataSuggestionProviderTest extends AbstractUnitTest {
             if (metadataValueDTO.getSchema().equals("dc") && metadataValueDTO.getElement().equals("identifier") &&
                 metadataValueDTO.getQualifier().equals("issn")) {
                 assertEquals(risSampleFileIssn, metadataValueDTO.getValue());
+            }
+        }
+    }
+
+
+    /**
+     * Test of {@link BTEMetadataSuggestionProvider#metadataQuery(Item, int, int)}
+     */
+    @Test
+    public void testMetadataQuery() {
+        String doiItemTitle = "Innovation in oncology clinical trial design.";
+        String doiItemSource = "Cancer treatment reviews";
+        List<ExternalDataObject> externalDataObjects =
+            this.bteMetadataSuggestionProvider.metadataQuery(this.doiItem, 0, 0);
+        assertEquals(doiItemTitle, externalDataObjects.get(0).getDisplayValue());
+        assertEquals(11, externalDataObjects.get(0).getMetadata().size());
+        for (MetadataValueDTO metadataValueDTO : externalDataObjects.get(0).getMetadata()) {
+            if (metadataValueDTO.getSchema().equals("dc") && metadataValueDTO.getElement().equals("title") &&
+                metadataValueDTO.getQualifier() == null) {
+                assertEquals(doiItemTitle, metadataValueDTO.getValue());
+            }
+            if (metadataValueDTO.getSchema().equals("dc") && metadataValueDTO.getElement().equals("source") &&
+                metadataValueDTO.getQualifier() == null) {
+                assertEquals(doiItemSource, metadataValueDTO.getValue());
             }
         }
     }
