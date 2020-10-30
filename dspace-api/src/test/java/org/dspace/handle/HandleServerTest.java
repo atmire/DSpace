8c8a45f6d1a31c087244d7d78d9c6d9a1ef3a66f
Ben Bosman <ben@atmire.com>
2020-10-30 09:43:11 +0100

    Verify handle server starts

---
+++ dspace-api/src/test/java/org/dspace/handle/HandleServerTest.java
@@ -0,0 +1,28 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.handle;
+
+import net.cnri.util.StreamTable;
+import net.handle.server.Main;
+import org.dspace.AbstractIntegrationTestWithDatabase;
+import org.junit.Test;
+
+import java.io.File;
+
+public class HandleServerTest
+        extends AbstractIntegrationTestWithDatabase {
+
+    @Test
+    public void testStartHandleServer() throws Exception {
+        StreamTable configTable = new StreamTable();
+        configTable.readFrom(getClass().getResourceAsStream("config.dct"));
+        Main main = new Main(new File(testProps.getProperty("test.folder")), configTable);
+        main.initialize();
+        //just test there's no exception
+    }
+}
