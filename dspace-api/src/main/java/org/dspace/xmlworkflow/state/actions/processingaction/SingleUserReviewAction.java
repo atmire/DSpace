5e1a456b5aa06eea700b4a8ecd7567a3474c1f0a
Marie Verdonck <maria.verdonck@atmire.com>
2023-02-09 11:03:24 +0100

    Alternate way to restart workflow in case of decline

--- dspace-api/src/main/java/org/dspace/xmlworkflow/state/actions/processingaction/SingleUserReviewAction.java
+++ dspace-api/src/main/java/org/dspace/xmlworkflow/state/actions/processingaction/SingleUserReviewAction.java
@@ -120,11 +120,8 @@ public class SingleUserReviewAction extends ProcessingAction {
      */
     private ActionResult processDecline(Context c, XmlWorkflowItem wfi)
         throws SQLException, IOException, AuthorizeException, WorkflowException {
-        EPerson user = c.getCurrentUser();
         c.turnOffAuthorisationSystem();
-        XmlWorkflowService xmlWorkflowService = XmlWorkflowServiceFactory.getInstance().getXmlWorkflowService();
-        WorkspaceItem workspaceItem = xmlWorkflowService.abort(c, wfi, user);
-        xmlWorkflowService.start(c, workspaceItem);
+        xmlWorkflowService.restartWorkflow(c, wfi, c.getCurrentUser(), this.getProvenanceStartId());
         c.restoreAuthSystemState();
         return new ActionResult(ActionResult.TYPE.TYPE_SUBMISSION_PAGE);
     }
