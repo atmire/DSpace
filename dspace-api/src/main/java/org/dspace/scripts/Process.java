dd9483de233eb6b30d6d75f927681f3a8d757771
Jens Vannerum <jens.vannerum@atmire.com>
2024-03-26 09:21:27 +0100

    Revert "113354: Lazy fetch the connector user to a process to avoid issues when fetching a process from a user that no longer exists"
    This reverts commit f2dc02c82335b47005eb6b3fa1b06ea84637b555.

--- dspace-api/src/main/java/org/dspace/scripts/Process.java
+++ dspace-api/src/main/java/org/dspace/scripts/Process.java
@@ -51,7 +51,7 @@ public class Process implements ReloadableEntity<Integer> {
     @Column(name = "process_id", unique = true, nullable = false)
     private Integer processId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private EPerson ePerson;
 
