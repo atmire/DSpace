bc6a96aae9ac3d43e88132633f8bcf1c924681d0
Bram Maegerman <bram.maegerman@telenet.be>
2026-03-30 15:53:50 +0200

    139748: added TOKEN_ADMIN permissions to process patch endpoint

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/ProcessRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/ProcessRestRepository.java
@@ -117,7 +117,7 @@ public class ProcessRestRepository extends DSpaceRestRepository<ProcessRest, Int
     }
 
     @Override
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TOKEN_ADMIN')")
     public void patch(Context context, HttpServletRequest request, String apiCategory, String model, Integer id,
                       Patch patch) {
         try {
